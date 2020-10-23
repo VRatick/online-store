1f41facf1f470245ea2e5c5f64bad6ff673e8986
DESKTOP-RE9G340\Ratick <v.ihnatii@gmail.com>
2020-10-23 13:55:41 +0300

    FIX:008 Fixes for duplicate items in the basket

--- src/src/redux/reducers/basket.js
+++ src/src/redux/reducers/basket.js
@@ -11,8 +11,17 @@ import {
     switch (action.type) {      
       case ADD_ITEM_TO_BASKET: {
         const { product } = action;
-        const products = state.products
+        const products = state.products;        
         products.push(product);
+        let count = 0;
+        products.forEach( (item, index) => {
+          if (product.uuid === item.uuid) {
+            count++;
+          }
+          if (count > 1) {
+            products.splice(index, 1);
+          }
+        })       
         return { ...state, products };
       }
       case REMOVE_ITEM_FROM_BASKET: {
