67d99d31f9df70e8b1b96ee40b08b7a14defcb13
DESKTOP-RE9G340\Ratick <v.ihnatii@gmail.com>
2020-10-23 13:22:43 +0300

    ADD:007 Adding the ability to select the number of items in the basket

--- src/src/screens/Basket.js
+++ src/src/screens/Basket.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { removeProduct } from '../redux/actions/basket';
+import { Select, MenuItem } from '@material-ui/core';
 
-function Basket (props) {
+function Basket (props) {    
     const products = props.products.length !== 0 ? props.products.map( (product) => {
         const count = 1;
+        let amount = [];         
+        for (let i = 1; i <= product.amount; i++) {
+            amount.push(<MenuItem value={i}>{i}</MenuItem>)
+        }        
+              
         return (
             <div key={product.uuid}>
                 <img src={product.image[0]} />
                 <h2>Name: {product.name}</h2>
-                <p>Amount: {count}</p>
+                <Select labelId="label" id={product.name} value="1">
+                    {amount}
+                </Select>
                 <p>Price: {product.price * count}</p>
                 <button onClick={ 
                         () => {
@@ -18,7 +26,7 @@ function Basket (props) {
                 }>Remove Product From Basket</button>
             </div>
         )
-    }) : <div>The basket is empty</div>;
+    }) : <div>The basket is empty</div>;    
     return (
         <div>
             {products}
